a6693a30051f196e69487445622ee12bf05093a8
FloatingRiver8 <maceiramariana@gmail.com>
2023-04-02 13:17:03 -0300

    changes in db settings, adding ssl attributes that was erased prevously by error

--- src/db.js
+++ src/db.js
@@ -15,6 +15,11 @@ const {
  const sequelize = new Sequelize(DB_DEPLOY, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false,
+  dialectOptions:{
+    ssl:{
+      require:true,
+    }
+  }
  // lets Sequelize know we can use pg-native for ~30% more speed
 }); 
 const basename = path.basename(__filename);
