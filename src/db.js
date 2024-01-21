684218556c54369bde6d90f3b29cf269a05c6c4c
FloatingRiver8 <maceiramariana@gmail.com>
2024-01-21 17:52:32 -0300

    trying to fix db connnect

--- src/db.js
+++ src/db.js
@@ -11,15 +11,17 @@ const {
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });  */
  
+
  const sequelize = new Sequelize(DB_DEPLOY, {
-  logging: false, // set to console.log to see the raw SQL queries
+  logging: false, 
   native: false,
   dialectOptions:{
     ssl:{
       require:true,
+      rejectUnauthorized: false,
     }
   }
- // lets Sequelize know we can use pg-native for ~30% more speed
+ 
 }); 
 const basename = path.basename(__filename);
 
